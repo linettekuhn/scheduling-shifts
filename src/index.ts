2c40eea4bc756f65b9608fb82de397bd683276d0
Linette Kühn <139412656+linettekuhn@users.noreply.github.com>
2026-05-06 21:44:38 -0400

    Add entry file index.ts

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import CaregiverRouter from "./routes/caregiver.routes";
+import PatientRouter from "./routes/patient.routes";
+import ShiftRouter from "./routes/shift.routes";
+import { env } from "./config/env";
+
+const app = express();
+
+// caregiver router
+app.use("/caregiver", CaregiverRouter);
+
+// patient router
+app.use("/patient", PatientRouter);
+
+// shift router
+app.use("/shift", ShiftRouter);
+
+// middleware to parse json
+app.use(express.json());
+
+// base route
+app.get("/", (req, res) => {
+  res.send("Scheduling backend running!");
+});
+
+app.listen(env.PORT, () => {
+  console.log(`Scheduling backend running on port ${env.PORT}`);
+});
